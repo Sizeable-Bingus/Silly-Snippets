f5b19152afae94bde1a8562da14d0d109602d020
Bingus <merrilltnewman@gmail.com>
2026-02-16 15:52:07 -0600

    Inline assembly example

---
+++ inlineAssembly/clr.zig
@@ -0,0 +1,133 @@
+const std = @import("std");
+const win = std.os.windows;
+
+pub const GUID = extern struct {
+    Data1: u32,
+    Data2: u16,
+    Data3: u16,
+    Data4: [8]u8,
+};
+
+pub const OLECHAR = u16;
+pub const VARTYPE = u16;
+
+pub const SAFEARRAY = extern struct {
+    _reserved: u8,
+};
+
+pub const SAFEARRAYBOUND = extern struct {
+    cElements: win.ULONG,
+    lLbound: win.LONG,
+};
+
+const variant_value = extern union {
+    parray: [*c]SAFEARRAY,
+};
+
+const variant_payload = extern struct {
+    vt: VARTYPE,
+    wReserved1: u16,
+    wReserved2: u16,
+    wReserved3: u16,
+    unnamed_0: variant_value,
+};
+
+const variant_union = extern union {
+    unnamed_0: variant_payload,
+};
+
+pub const VARIANT = extern struct {
+    unnamed_0: variant_union,
+};
+
+pub const VARIANTARG = VARIANT;
+
+pub const VT_NULL: VARTYPE = 1;
+pub const VT_BSTR: VARTYPE = 8;
+pub const VT_VARIANT: VARTYPE = 12;
+pub const VT_UI1: VARTYPE = 17;
+pub const VT_ARRAY: VARTYPE = 0x2000;
+
+pub const IUnknown = extern struct {
+    lpVtbl: [*c]IUnknownVtbl,
+};
+
+pub const IUnknownVtbl = extern struct {
+    QueryInterface: ?*const fn ([*c]IUnknown, [*c]const GUID, [*c]?*anyopaque) callconv(.c) win.HRESULT,
+    AddRef: ?*const fn ([*c]IUnknown) callconv(.c) win.ULONG,
+    Release: ?*const fn ([*c]IUnknown) callconv(.c) win.ULONG,
+};
+
+pub const ICLRMetaHost = extern struct {
+    lpVtbl: [*c]ICLRMetaHostVtbl,
+};
+
+pub const ICLRMetaHostVtbl = extern struct {
+    QueryInterface: ?*const fn ([*c]ICLRMetaHost, [*c]const GUID, [*c]?*anyopaque) callconv(.c) win.HRESULT,
+    AddRef: ?*const fn ([*c]ICLRMetaHost) callconv(.c) win.ULONG,
+    Release: ?*const fn ([*c]ICLRMetaHost) callconv(.c) win.ULONG,
+    GetRuntime: ?*const fn ([*c]ICLRMetaHost, win.LPCWSTR, [*c]const GUID, [*c]win.LPVOID) callconv(.c) win.HRESULT,
+};
+
+pub const ICLRRuntimeInfo = extern struct {
+    lpVtbl: [*c]ICLRRuntimeInfoVtbl,
+};
+
+pub const ICLRRuntimeInfoVtbl = extern struct {
+    QueryInterface: ?*const fn ([*c]ICLRRuntimeInfo, [*c]const GUID, [*c]?*anyopaque) callconv(.c) win.HRESULT,
+    AddRef: ?*const fn ([*c]ICLRRuntimeInfo) callconv(.c) win.ULONG,
+    Release: ?*const fn ([*c]ICLRRuntimeInfo) callconv(.c) win.ULONG,
+    _reserved3_to_8: [6]?*const anyopaque,
+    GetInterface: ?*const fn ([*c]ICLRRuntimeInfo, [*c]const GUID, [*c]const GUID, [*c]win.LPVOID) callconv(.c) win.HRESULT,
+    IsLoadable: ?*const fn ([*c]ICLRRuntimeInfo, [*c]win.BOOL) callconv(.c) win.HRESULT,
+};
+
+pub const ICorRuntimeHost = extern struct {
+    lpVtbl: [*c]ICorRuntimeHostVtbl,
+};
+
+pub const ICorRuntimeHostVtbl = extern struct {
+    QueryInterface: ?*const fn ([*c]ICorRuntimeHost, [*c]const GUID, [*c]?*anyopaque) callconv(.c) win.HRESULT,
+    AddRef: ?*const fn ([*c]ICorRuntimeHost) callconv(.c) win.ULONG,
+    Release: ?*const fn ([*c]ICorRuntimeHost) callconv(.c) win.ULONG,
+    _reserved3_to_9: [7]?*const anyopaque,
+    Start: ?*const fn ([*c]ICorRuntimeHost) callconv(.c) win.HRESULT,
+    _reserved11: ?*const anyopaque,
+    CreateDomain: ?*const fn ([*c]ICorRuntimeHost, win.LPCWSTR, [*c]IUnknown, [*c][*c]IUnknown) callconv(.c) win.HRESULT,
+};
+
+pub const AppDomain = extern struct {
+    lpVtbl: [*c]AppDomainVtbl,
+};
+
+pub const AppDomainVtbl = extern struct {
+    QueryInterface: ?*const fn ([*c]AppDomain, [*c]const GUID, [*c]?*anyopaque) callconv(.c) win.HRESULT,
+    AddRef: ?*const fn ([*c]AppDomain) callconv(.c) win.ULONG,
+    Release: ?*const fn ([*c]AppDomain) callconv(.c) win.ULONG,
+    _reserved3_to_44: [42]?*const anyopaque,
+    Load_3: ?*const fn ([*c]AppDomain, [*c]SAFEARRAY, [*c][*c]Assembly) callconv(.c) win.HRESULT,
+};
+
+pub const Assembly = extern struct {
+    lpVtbl: [*c]AssemblyVtbl,
+};
+
+pub const AssemblyVtbl = extern struct {
+    QueryInterface: ?*const fn ([*c]Assembly, [*c]const GUID, [*c]?*anyopaque) callconv(.c) win.HRESULT,
+    AddRef: ?*const fn ([*c]Assembly) callconv(.c) win.ULONG,
+    Release: ?*const fn ([*c]Assembly) callconv(.c) win.ULONG,
+    _reserved3_to_15: [13]?*const anyopaque,
+    EntryPoint: ?*const fn ([*c]Assembly, [*c][*c]MethodInfo) callconv(.c) win.HRESULT,
+};
+
+pub const MethodInfo = extern struct {
+    lpVtbl: [*c]MethodInfoVtbl,
+};
+
+pub const MethodInfoVtbl = extern struct {
+    QueryInterface: ?*const fn ([*c]MethodInfo, [*c]const GUID, [*c]?*anyopaque) callconv(.c) win.HRESULT,
+    AddRef: ?*const fn ([*c]MethodInfo) callconv(.c) win.ULONG,
+    Release: ?*const fn ([*c]MethodInfo) callconv(.c) win.ULONG,
+    _reserved3_to_36: [34]?*const anyopaque,
+    Invoke_3: ?*const fn ([*c]MethodInfo, VARIANT, [*c]SAFEARRAY, [*c]VARIANT) callconv(.c) win.HRESULT,
+};
---
+++ inlineAssembly/inlineAssembly.zig
@@ -0,0 +1,276 @@
+const std = @import("std");
+const clr = @import("clr.zig");
+const win = std.os.windows;
+
+extern "shell32" fn CommandLineToArgvW(lpCmdLine: win.LPWSTR, pNumArgs: *c_int) [*]win.LPWSTR;
+extern "oleaut32" fn SafeArrayCreateVector(vt: clr.VARTYPE, lower_bound: win.LONG, count: win.ULONG) ?*clr.SAFEARRAY;
+extern "oleaut32" fn SafeArrayPutElement(psa: *clr.SAFEARRAY, rgIndices: *win.LONG, pv: *anyopaque) win.HRESULT;
+extern "oleaut32" fn SysAllocString(psz: ?[*]clr.OLECHAR) win.BSTR;
+extern "oleaut32" fn SafeArrayCreate(vt: clr.VARTYPE, cDims: win.UINT, rgsabound: [*]clr.SAFEARRAYBOUND) callconv(.winapi) ?*clr.SAFEARRAY;
+extern "oleaut32" fn SafeArrayAccessData(psa: *clr.SAFEARRAY, ppvData: **anyopaque) win.HRESULT;
+extern "oleaut32" fn SafeArrayUnaccessData(psa: *clr.SAFEARRAY) win.HRESULT;
+extern "oleaut32" fn SafeArrayDestroy(psa: *clr.SAFEARRAY) win.HRESULT;
+extern "oleaut32" fn VariantClear(pvarg: *clr.VARIANTARG) win.HRESULT;
+
+const fnCLRCreateInstance = *const fn (
+    clsid: *const clr.GUID,
+    riid: *const clr.GUID,
+    ppInterface: *?*anyopaque,
+) callconv(.winapi) win.HRESULT;
+
+const xCLSID_CorRuntimeHost: clr.GUID = .{
+    .Data1 = 0xcb2f6723,
+    .Data2 = 0xab3a,
+    .Data3 = 0x11d2,
+    .Data4 = .{ 0x9c, 0x40, 0x00, 0xc0, 0x4f, 0xa3, 0x0a, 0x3e },
+};
+
+const xIID_ICorRuntimeHost: clr.GUID = .{
+    .Data1 = 0xcb2f6722,
+    .Data2 = 0xab3a,
+    .Data3 = 0x11d2,
+    .Data4 = .{ 0x9c, 0x40, 0x00, 0xc0, 0x4f, 0xa3, 0x0a, 0x3e },
+};
+
+const xIID_AppDomain: clr.GUID = .{
+    .Data1 = 0x05F696DC,
+    .Data2 = 0x2B29,
+    .Data3 = 0x3663,
+    .Data4 = .{ 0xAD, 0x8B, 0xC4, 0x38, 0x9C, 0xF2, 0xA7, 0x13 },
+};
+
+const xCLSID_CLRMetaHost: clr.GUID = .{
+    .Data1 = 0x9280188d,
+    .Data2 = 0x0e8e,
+    .Data3 = 0x4867,
+    .Data4 = .{ 0xb3, 0x0c, 0x7f, 0xa8, 0x38, 0x84, 0xe8, 0xde },
+};
+
+const xIID_ICLRMetaHost: clr.GUID = .{
+    .Data1 = 0xD332DB9E,
+    .Data2 = 0xB9B3,
+    .Data3 = 0x4125,
+    .Data4 = .{ 0x82, 0x07, 0xA1, 0x48, 0x84, 0xF5, 0x32, 0x16 },
+};
+
+const xIID_ICLRRuntimeInfo: clr.GUID = .{
+    .Data1 = 0xBD39D1D2,
+    .Data2 = 0xBA2F,
+    .Data3 = 0x486A,
+    .Data4 = .{ 0x89, 0xB0, 0xB4, 0xB0, 0xCB, 0x46, 0x68, 0x91 },
+};
+
+fn utf8ToUtf16LeSentinel(allocator: std.mem.Allocator, utf8: []const u8) ![:0]const u16 {
+    const utf16_buf = try allocator.alloc(u16, utf8.len + 1);
+    const utf16_end = try std.unicode.utf8ToUtf16Le(utf16_buf, utf8);
+    utf16_buf[utf16_end] = 0;
+    return utf16_buf[0 .. utf16_buf.len - 1 :0];
+}
+
+const ReadFileError = error{FileToLarge} || std.fs.File.OpenError || std.fs.File.Reader.SizeError || std.Io.Reader.Error || std.mem.Allocator.Error;
+fn readFile(allocator: std.mem.Allocator, file_path: []const u8) ReadFileError![]const u8 {
+    const file = try std.fs.cwd().openFile(file_path, .{});
+
+    var reader_buf: [1024]u8 = undefined;
+    var reader: std.fs.File.Reader = file.reader(&reader_buf);
+
+    const file_size = try reader.getSize();
+    if (file_size > 1024 * 1024) return error.FileToLarge;
+
+    const file_buf = try allocator.alloc(u8, file_size);
+
+    try reader.interface.readSliceAll(file_buf);
+
+    return file_buf;
+}
+
+fn findVersion(assembly: []const u8) bool { // this doesnt work right
+    const v4 = &.{ 0x76, 0x34, 0x2E, 0x30, 0x2E, 0x33, 0x30, 0x33, 0x31, 0x39 };
+
+    for (assembly, 0..) |_, i| {
+        if (std.mem.eql(u8, assembly[i..], v4)) {
+            return true;
+        }
+    }
+    //return false;
+    return true;
+}
+
+fn startCLR(dotnet_version: win.LPCWSTR, ppClrMetaHost: **clr.ICLRMetaHost, ppClrRuntimeInfo: **clr.ICLRRuntimeInfo, ppICorRuntimeHost: **clr.ICorRuntimeHost) !bool {
+    var dll = try std.DynLib.open("mscoree.dll");
+    defer dll.close();
+
+    const CLRCreateInstance = dll.lookup(fnCLRCreateInstance, "CLRCreateInstance") orelse
+        return error.SymbolNotFound;
+
+    var hr: win.HRESULT = CLRCreateInstance(&xCLSID_CLRMetaHost, &xIID_ICLRMetaHost, @ptrCast(ppClrMetaHost));
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:CLRCreateInstance] : {}\n", .{hr});
+    }
+
+    const w_dotnet_version = std.mem.span(dotnet_version);
+    var a_dotnet_version_buf: [1024]u8 = undefined;
+    const a_dotnet_version_end = try std.unicode.utf16LeToUtf8(&a_dotnet_version_buf, w_dotnet_version);
+    const a_dotnet_version = a_dotnet_version_buf[0..a_dotnet_version_end];
+
+    const metahost_vt = &ppClrMetaHost.*.*.lpVtbl.?.*;
+    std.debug.print("[startCLR:GetRuntime] {s} : {}\n", .{ a_dotnet_version, ppClrRuntimeInfo });
+    //hr = ppClrMetaHost.*.*.lpVtbl.?.*.GetRuntime.?(ppClrMetaHost.*, dotnet_version, &metahost.IID_ICLRRuntimeInfo, @ptrCast(ppClrRuntimeInfo));
+    hr = metahost_vt.*.GetRuntime.?(ppClrMetaHost.*, dotnet_version, &xIID_ICLRRuntimeInfo, @ptrCast(ppClrRuntimeInfo));
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:GetRuntime] : {}\n", .{hr});
+        return false;
+    }
+
+    var loadable: win.BOOL = win.FALSE;
+    hr = ppClrRuntimeInfo.*.lpVtbl.?.*.IsLoadable.?(ppClrRuntimeInfo.*, &loadable);
+    if (loadable != win.TRUE or hr != win.S_OK) {
+        std.debug.print("[ERR:IsLoadable] : {} : {}\n", .{ loadable, hr });
+        return false;
+    }
+
+    hr = ppClrRuntimeInfo.*.lpVtbl.?.*.GetInterface.?(ppClrRuntimeInfo.*, &xCLSID_CorRuntimeHost, &xIID_ICorRuntimeHost, @ptrCast(ppICorRuntimeHost));
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:GetInterface] : {}\n", .{hr});
+        return false;
+    }
+
+    hr = ppICorRuntimeHost.*.lpVtbl.?.*.Start.?(ppICorRuntimeHost.*);
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:Start] : {}\n", .{hr});
+        return false;
+    }
+
+    std.debug.print("[startCLR] CLR Instance Created\n", .{});
+    return true;
+}
+
+fn executeAssembly(allocator: std.mem.Allocator, assembly: []const u8, args: []const u8) !void {
+    var dotnet_version: []const u8 = undefined;
+    if (findVersion(assembly)) {
+        std.debug.print("V4\n", .{});
+        dotnet_version = "v4.0.30319";
+    } else {
+        std.debug.print("V2\n", .{});
+        dotnet_version = "v2.0.50727";
+    }
+    const w_dotnet_version_buf = try allocator.alloc(u16, dotnet_version.len + 1);
+    defer allocator.free(w_dotnet_version_buf);
+    const w_dotnet_version_buf_end = try std.unicode.utf8ToUtf16Le(w_dotnet_version_buf, dotnet_version);
+    w_dotnet_version_buf[w_dotnet_version_buf_end] = 0;
+    const w_dotnet_version = w_dotnet_version_buf[0 .. w_dotnet_version_buf.len - 1 :0];
+
+    var w_args_buf: []u16 = try allocator.alloc(u16, args.len + 1);
+    defer allocator.free(w_args_buf);
+    const w_args_end = try std.unicode.utf8ToUtf16Le(w_args_buf, args);
+    w_args_buf[w_args_end] = 0;
+    const w_args = w_args_buf[0 .. w_args_buf.len - 1 :0];
+
+    var num_args: c_int = 0;
+    const args_array = CommandLineToArgvW(w_args.ptr, &num_args);
+    std.debug.print("num_args : {d}\n", .{num_args});
+
+    var vt_psa: clr.VARIANT = std.mem.zeroes(clr.VARIANT);
+    vt_psa.unnamed_0.unnamed_0.vt = (clr.VT_ARRAY | clr.VT_BSTR);
+    vt_psa.unnamed_0.unnamed_0.unnamed_0.parray = SafeArrayCreateVector(clr.VT_BSTR, 0, @intCast(num_args));
+    if (vt_psa.unnamed_0.unnamed_0.unnamed_0.parray == null) {
+        std.debug.print("[ERR:SafeArrayCreateVector]\n", .{});
+        return;
+    }
+
+    var i: win.LONG = 0;
+    while (i < num_args) : (i += 1) {
+        const a_str = SysAllocString(args_array[@intCast(i)]);
+        if (SafeArrayPutElement(vt_psa.unnamed_0.unnamed_0.unnamed_0.parray, &i, a_str) != win.S_OK) {
+            std.debug.print("[ERR:SafeArrayPutElement]\n", .{});
+        }
+    }
+
+    var pCLRMetaHost: *clr.ICLRMetaHost = undefined;
+    var pCLRRuntimeInfo: *clr.ICLRRuntimeInfo = undefined;
+    var pCORRuntimeHost: *clr.ICorRuntimeHost = undefined;
+    _ = try startCLR(w_dotnet_version, &pCLRMetaHost, &pCLRRuntimeInfo, &pCORRuntimeHost);
+
+    const w_appdomain = try utf8ToUtf16LeSentinel(allocator, "silly_domain");
+    defer allocator.free(w_appdomain);
+    var appdomain_thunk: *clr.IUnknown = undefined;
+    var hr: win.HRESULT = pCORRuntimeHost.*.lpVtbl.?.*.CreateDomain.?(pCORRuntimeHost, w_appdomain, null, @ptrCast(&appdomain_thunk));
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:CreateDomain] : {}\n", .{hr});
+    }
+    var appdomain: *clr.AppDomain = undefined;
+    hr = appdomain_thunk.*.lpVtbl.?.*.QueryInterface.?(appdomain_thunk, &xIID_AppDomain, @ptrCast(&appdomain));
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:QueryInterface] : {}\n", .{hr});
+    }
+
+    var rgsabound: [1]clr.SAFEARRAYBOUND = undefined;
+    rgsabound[0].cElements = @intCast(assembly.len);
+    rgsabound[0].lLbound = 0;
+    var pv_data: *anyopaque = undefined;
+    const safe_array = SafeArrayCreate(clr.VT_UI1, 1, &rgsabound).?;
+    hr = SafeArrayAccessData(safe_array, &pv_data);
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:SafeArrayAccessData] : {}\n", .{hr});
+    }
+
+    @memcpy(@as([*]u8, @ptrCast(pv_data)), assembly);
+
+    hr = SafeArrayUnaccessData(safe_array);
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:SafeArrayUnAccessData] : {}\n", .{hr});
+    }
+
+    var loaded_assembly: *clr.Assembly = undefined;
+    hr = appdomain.lpVtbl.?.*.Load_3.?(appdomain, safe_array, @ptrCast(&loaded_assembly));
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:Load_3] : {}\n", .{hr});
+    }
+
+    var method_info: *clr.MethodInfo = undefined;
+    hr = loaded_assembly.lpVtbl.?.*.EntryPoint.?(loaded_assembly, @ptrCast(&method_info));
+    if (hr != win.S_OK) {
+        std.debug.print("[ERR:EntryPoint] : {}\n", .{hr});
+    }
+
+    var ret_val = std.mem.zeroes(clr.VARIANT);
+    var obj = std.mem.zeroes(clr.VARIANT);
+    obj.unnamed_0.unnamed_0.vt = clr.VT_NULL;
+
+    const psa_method_args = SafeArrayCreateVector(clr.VT_VARIANT, 0, 1).?;
+    var idx: i32 = 0;
+    _ = SafeArrayPutElement(psa_method_args, &idx, &vt_psa);
+
+    hr = method_info.lpVtbl.*.Invoke_3.?(method_info, obj, @ptrCast(psa_method_args), @ptrCast(&ret_val));
+
+    //TODO: read output
+
+    _ = SafeArrayDestroy(safe_array);
+    _ = VariantClear(&ret_val);
+    _ = VariantClear(&obj);
+    _ = VariantClear(&vt_psa);
+
+    _ = SafeArrayDestroy(psa_method_args);
+    _ = method_info.lpVtbl.*.Release.?(method_info);
+    _ = loaded_assembly.lpVtbl.*.Release.?(loaded_assembly);
+    _ = appdomain.lpVtbl.*.Release.?(appdomain);
+    _ = appdomain_thunk.lpVtbl.*.Release.?(appdomain_thunk);
+    _ = pCORRuntimeHost.lpVtbl.*.Release.?(pCORRuntimeHost);
+    _ = pCLRRuntimeInfo.lpVtbl.*.Release.?(pCLRRuntimeInfo);
+    _ = pCLRMetaHost.lpVtbl.*.Release.?(pCLRMetaHost);
+}
+
+pub fn main() !void {
+    var debug_alloc = std.heap.DebugAllocator(.{}){};
+    defer std.debug.assert(debug_alloc.deinit() == .ok);
+    const dba = debug_alloc.allocator();
+
+    const args = try std.process.argsAlloc(dba);
+    defer std.process.argsFree(dba, args);
+    const dotnet_file_path = args[1];
+
+    const dotnet = try readFile(dba, dotnet_file_path);
+    defer dba.free(dotnet);
+
+    try executeAssembly(dba, dotnet, "zyx");
+}
