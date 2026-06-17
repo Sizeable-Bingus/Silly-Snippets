e1e1f77f1b8b055a890cb34091f396b69598cc10
Bingus <merrilltnewman@gmail.com>
2026-06-17 17:16:39 -0500

    Update samples

--- CET_APC_no_gadget.c
+++ CET_APC_no_gadget.c
@@ -1,70 +1,19 @@
 #include <windows.h>
+#include <ntstatus.h>
 #include <stdio.h>
 
-// Parasitic gadget
 
 typedef BOOL (WINAPI* QueueUserAPC2_t)(PAPCFUNC ApcRoutine, HANDLE Thread, ULONG_PTR Data, QUEUE_USER_APC_FLAGS Flags);
-//   rsi = call target
-//   rbx = pointer to final jmp slot   (jmp reads the target from [rbx])
-//
-//   FF D6   call rsi
-//   FF 23   jmp  qword [rbx]
-static unsigned char g_stub[] = {
-    0xFF, 0xD6,   // call rsi
-    0xFF, 0x23,   // jmp [rbx]
-};
+typedef NTSTATUS (NTAPI* NtGetNextThread_t)(
+    _In_ HANDLE ProcessHandle,
+    _In_opt_ HANDLE ThreadHandle,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_ ULONG HandleAttributes,
+    _In_opt_ _Reserved_ ULONG Flags,
+    _Out_ PHANDLE NewThreadHandle
+    );
 
 #define STUB_LEN (sizeof(g_stub))   // 4
-
-typedef struct _STUB_CALL_ARGS {
-    UINT_PTR arg1;
-    UINT_PTR arg2;
-    UINT_PTR arg3;
-    UINT_PTR arg4;
-} STUB_CALL_ARGS;
-
-typedef struct _STUB_ARG {
-  UINT_PTR p_stub;        // 0
-  UINT_PTR p_call;        // 8
-  UINT_PTR p_jump;        // 16
-  UINT_PTR p_rbx;         // 24
-  UINT_PTR p_rsi;         // 32
-  UINT_PTR p_rdi;         // 40
-  UINT_PTR lpFiber;       // 48
-  STUB_CALL_ARGS args;    // 56
-} STUB_ARG;
-
-__attribute__((naked))
-void stubSetup(PAPC_CALLBACK_DATA arg) {
-    __asm(//"sub rsp, 8 \n"
-        "sub rsp, 0xA8 \n"
-        "mov r10, [rcx] \n"
-        "mov [r10 + 24], rbx \n"      // save -> p_rbx
-        "mov [r10 + 32], rsi \n"      // save -> p_rsi
-        "mov [r10 + 40], rdi \n"      // save -> p_rdi
-
-        "mov rdi, r10 \n"             // arg ptr (survives call)
-        "lea rbx, [r10 + 16] \n"      // &p_jump ; stub's jmp [rbx] -> stubReturn
-        "mov rsi, [r10 + 8] \n"       // p_call
-
-        "mov r11, r10\n"
-        "mov rdx, [r10 + 64] \n"      // arg2
-        "mov r8,  [r10 + 72] \n"      // arg3
-        "mov r9,  [r10 + 80] \n"      // arg4
-        "mov rcx, [r10 + 56] \n"      // arg1
-
-        "jmp [r11] \n");
-}
-
-__attribute__((naked))
-void stubReturn() {
-    __asm("add rsp, 0xA8 \n"
-        "mov rbx, [rdi + 24] \n"      // restore p_rbx
-        "mov rsi, [rdi + 32] \n"      // restore p_rsi
-        "mov rdi, [rdi + 40] \n"      // restore p_rdi
-
-        "ret \n");
-}
 
 __attribute__((naked))
 void sleepExSetup() {
@@ -74,25 +23,13 @@ void sleepExSetup() {
 }
 
 __attribute__((naked))
-void sleepExSetupReturn() {
-    __asm("add rsp, 0xA8\n"
-        "mov rcx, 0xffffffff\n"
-        "mov rdx, 1\n"
-        "jmp SleepEx\n");
-}
-
-__attribute__((naked))
 void intteruptStub(PAPC_CALLBACK_DATA data) {
     __asm(
-        "mov r10, [rcx]\n"        // &sleep_context
-        "mov r11, [rcx + 8]\n"    // &ContextRecord
-        "mov rsi, r10\n"
-        "mov rdi, r11\n"
-        "mov rcx, 154\n"
-        "rep movsq\n"
-
+        "mov r10, [rcx]\n"     // Parameter
+        "mov r11, [rcx + 8]\n" // ContextRecord
+        
         //"mov qword ptr [r11 + 0x4F0], 0\n"
-        //"mov qword ptr [r11 +0x78], 0x101\n" // Have RtlDelayExecution return with STATUS_ALERTED
+        "mov qword ptr [r11 +0x78], 0x101\n" // Have RtlDelayExecution return with STATUS_ALERTED
 
         "mov rcx, 0\n"
         "mov rdx, 0\n"
@@ -101,49 +38,34 @@ void intteruptStub(PAPC_CALLBACK_DATA data) {
         "jmp MessageBoxA\n");
 }
 
-static VOID sleepRoutine(PAPC_CALLBACK_DATA data) {
-    while (TRUE) {
-        if (SleepEx(INFINITE, TRUE) == WAIT_IO_COMPLETION) continue;
-    }
-}
-
-static VOID interruptRoutine(PAPC_CALLBACK_DATA data) {
-    printf("[APC] 0x%llx : 0x%llx", data->ContextRecord->Rip,
-                                    data->ContextRecord->Rsp);
-    MessageBoxA(NULL, "Interrupt", "Interrupt", MB_OK);
-}
-
-typedef struct _INTERRUPT_ARG {
-    UINT_PTR Rip;
-    UINT_PTR Rsp;
-} INTERRUPT_ARG;
-
-void stitch(HMODULE h_mod, HANDLE h_thread, CONTEXT sleep_context) {
-    // STUB_CALL_ARGS call_args = { .arg1 = 0, .arg2 = 0, .arg3 = 0, .arg4 = MB_OK };
-    // STUB_ARG stub_arg = { 
-    //     .p_stub = (UINT_PTR)h_mod,
-    //     .p_call = (UINT_PTR)MessageBoxA,
-    //     .p_jump = (UINT_PTR)sleepExSetupReturn,
-    //     .p_rbx = 0,
-    //     .p_rsi = 0,
-    //     .p_rdi = 0,
-    //     .lpFiber = (UINT_PTR)0,
-    //     .args = call_args,
-    // };
-
-    //INTERRUPT_ARG arg = { .Rip = sleep_context.Rip, .Rsp = sleep_context.Rsp };
-    
-
-    printf("[CTX] 0x%llx : 0x%llx\n", sleep_context.Rip, sleep_context.Rsp);
+void stitch(HANDLE h_thread) {
     printf("[+] Queueing APC\n");
-    if (!QueueUserAPC2((PAPCFUNC)intteruptStub, h_thread, (ULONG_PTR)&sleep_context, QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC | QUEUE_USER_APC_CALLBACK_DATA_CONTEXT)) {
+    if (!QueueUserAPC2((PAPCFUNC)intteruptStub, h_thread, 0, QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC | QUEUE_USER_APC_CALLBACK_DATA_CONTEXT)) {
         printf("[ERR:GetProcAddress]\n");
     }
     getchar();
-    // SuspendThread(h_thread);
-    // SetThreadContext(h_thread, &sleep_context);
-    // ResumeThread(h_thread);
-    // getchar();
+}
+
+NTSTATUS enumThreads(HANDLE h_proc) {
+    NtGetNextThread_t NtGetNextThread = (NtGetNextThread_t)GetProcAddress(
+        GetModuleHandle("ntdll.dll"),
+        "NtGetNextThread"
+    );
+    if (NtGetNextThread == NULL) {
+        printf("[ERR:GetProcAddress]\n");
+        return 1;
+    }
+
+    NTSTATUS status = STATUS_SUCCESS;
+    HANDLE h_thread = NULL;
+    status = NtGetNextThread(h_proc, NULL, READ_CONTROL, 0, 0, &h_thread);
+    if (status != STATUS_SUCCESS) return status;
+
+    while (status != STATUS_NO_MORE_ENTRIES) {
+        printf("[enumThreads] 0x%p\n", h_thread);
+        status = NtGetNextThread(h_proc, h_thread, READ_CONTROL, 0, 0, &h_thread);
+    }
+    return status;
 }
 
 int main() {
@@ -156,26 +78,18 @@ int main() {
         return 1;
     }
 
-    HMODULE hMod = LoadLibraryA("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\149.0.4022.62\\msedge_elf.dll");
-    if (hMod == NULL) return 1;
-    printf("0x%p\n", hMod);
-    hMod = (HMODULE)((UINT_PTR)hMod + 0x13CAF);
-    printf("0x%p\n", hMod);
+    //HANDLE h_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)sleepExSetup, NULL, 0, NULL);
+    //Sleep(500);
+
+    // Avoid thread creation and RtlTimer* in callstack but introduce UB
+    // Technically UB but from what I can see RDX is always SleepEx which is true
+    // Identifying this thread is tricky...
+    HANDLE h_timer = 0;
+    QueueUserWorkItem((LPTHREAD_START_ROUTINE)SleepEx, (PVOID)INFINITE, 0);
+    getchar();
+    enumThreads(GetCurrentProcess());
+    return 0;
     
-    DWORD dwOld;
-    VirtualProtect(hMod, 4, PAGE_EXECUTE_READWRITE, &dwOld);
-    memcpy(hMod, g_stub, 4);
-    printf("0x%p\n", hMod);
-
-    HANDLE h_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)sleepExSetup, NULL, 0, NULL);
-    Sleep(500);
-
-    CONTEXT sleep_context = { 0 };
-    sleep_context.ContextFlags = CONTEXT_FULL;
-    SuspendThread(h_thread);
-    GetThreadContext(h_thread, &sleep_context);
-    ResumeThread(h_thread);
-
-    stitch(hMod, h_thread, sleep_context);
-    stitch(hMod, h_thread, sleep_context);
+    // stitch(h_thread);
+    // stitch(h_thread);
 }
--- unwind.cpp
+++ unwind.cpp
@@ -5,14 +5,28 @@
 #include <processthreadsapi.h>
 #include <psapi.h>
 #include <dbghelp.h>
+#include <winternl.h>
+#include <ntstatus.h>
 
 #include <array>
+#include <vector>
+#include <expected>
 #include <cstdlib>
 #include <format>
 #include <print>
 #include <iostream>
 #include <memory>
 #include <string_view>
+
+typedef NTSTATUS (NTAPI* NtGetNextThread_t)(
+    _In_ HANDLE ProcessHandle,
+    _In_opt_ HANDLE ThreadHandle,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_ ULONG HandleAttributes,
+    _In_opt_ _Reserved_ ULONG Flags,
+    _Out_ PHANDLE NewThreadHandle
+    );
+
 
 namespace {
 
@@ -24,20 +38,133 @@ struct HandleDeleter {
     }
 };
 
-using UniqueHandle = std::unique_ptr<void, HandleDeleter>;
 
-[[nodiscard]] UniqueHandle open_process(DWORD access, DWORD pid) {
-    return UniqueHandle{OpenProcess(access, FALSE, pid)};
-}
+using uniq_handle = std::unique_ptr<void, HandleDeleter>;
 
-[[nodiscard]] UniqueHandle open_thread(DWORD access, DWORD tid) {
-    return UniqueHandle{OpenThread(access, FALSE, tid)};
-}
+struct ProcessThreads {
+    HANDLE h_process;
+    std::vector<uniq_handle> h_threads;
+};
+
 
 [[noreturn]] void die(std::string_view api) {
-    std::cerr << std::format("[ERR:{}] {}\n", api, GetLastError());
+    std::cerr << std::format("[ERR:{}] {:#x}\n", api, GetLastError());
     std::exit(EXIT_FAILURE);
 }
+
+void print_frame(HANDLE process, DWORD64 address) {
+    alignas(SYMBOL_INFO)
+    std::array<CHAR, sizeof(SYMBOL_INFO) + (MAX_PATH + 1) * sizeof(CHAR)> symbol_buff{};
+    auto* symbol = reinterpret_cast<PSYMBOL_INFO>(symbol_buff.data());
+    symbol->MaxNameLen = MAX_PATH;
+    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+
+    DWORD64 disp{};
+    IMAGEHLP_MODULE64 mod{};
+    mod.SizeOfStruct = sizeof(mod);
+    const bool have_mod = SymGetModuleInfo64(process, address, &mod);
+
+    if (SymFromAddr(process, address, &disp, symbol)) {
+        if (have_mod)
+            std::println("{}!{}+0x{:x}", mod.ModuleName, symbol->Name, disp);
+        else
+            std::println("<UNKNOWN>!{}+0x{:x}", symbol->Name, disp);
+    } else if (have_mod) {
+        std::println("{}+{:#x}", mod.ModuleName, address - mod.BaseOfImage);
+    } else {
+        std::println("{:#x}", address);
+    }
+}
+
+std::expected<ProcessThreads, NTSTATUS> enumThreads(HANDLE h_proc) {
+    NtGetNextThread_t NtGetNextThread = (NtGetNextThread_t)GetProcAddress(
+        GetModuleHandle("ntdll.dll"),
+        "NtGetNextThread"
+    );
+    if (NtGetNextThread == NULL) {
+        std::println("[ERR:GetProcAddress]\n");
+        return std::unexpected(STATUS_NOT_FOUND);
+    }
+
+    NTSTATUS status = STATUS_SUCCESS;
+    HANDLE h_thread = NULL;
+    status = NtGetNextThread(h_proc, NULL, THREAD_ALL_ACCESS, 0, 0, &h_thread);
+    if (status != STATUS_SUCCESS) return std::unexpected(status);
+
+    ProcessThreads process_threads{
+        .h_process = h_proc,
+        .h_threads{},
+    };
+
+    process_threads.h_threads.push_back(uniq_handle{h_thread});
+    while (status != STATUS_NO_MORE_ENTRIES) {
+        status = NtGetNextThread(h_proc, h_thread, THREAD_ALL_ACCESS, 0, 0, &h_thread);
+
+        if (status == STATUS_SUCCESS) process_threads.h_threads.push_back(uniq_handle{h_thread});
+        else if (status != STATUS_SUCCESS && status != STATUS_NO_MORE_ENTRIES) return std::unexpected(status);
+    }
+    return process_threads;
+ 
+}
+
+std::expected<void, NTSTATUS> enumThreadStacks(ProcessThreads process_threads) {
+    for (const auto& h_thread: process_threads.h_threads) {
+        auto thread_id = GetThreadId(h_thread.get());
+        std::println("\n===================={}====================", thread_id);
+        CONTEXT ctx{};
+        ctx.ContextFlags = CONTEXT_FULL;
+        if (!GetThreadContext(h_thread.get(), &ctx)) {
+            die("GetThreadContext");
+        }
+
+        STACKFRAME64 frame{};
+        frame.AddrPC.Offset = ctx.Rip;
+        frame.AddrFrame.Offset = ctx.Rbp;
+        frame.AddrStack.Offset = ctx.Rsp;
+        frame.AddrPC.Mode = AddrModeFlat;
+        frame.AddrFrame.Mode = AddrModeFlat;
+        frame.AddrStack.Mode = AddrModeFlat;
+
+        while (true) {
+            if (!StackWalk64(IMAGE_FILE_MACHINE_AMD64,
+                             process_threads.h_process,
+                             h_thread.get(),
+                             &frame,
+                             &ctx,
+                             nullptr,
+                             SymFunctionTableAccess64,
+                             SymGetModuleBase64,
+                             nullptr))
+                break;
+
+            const auto address = frame.AddrPC.Offset;
+            if (address == 0) break;
+
+            print_frame(process_threads.h_process, address);
+        }
+        std::println("===================={}====================", thread_id);
+    }
+    return{};
+}
+
+
+// [[nodiscard]] DWORD64 thread_start_address(HANDLE thread) {
+//     using Fn = NTSTATUS(NTAPI*)(HANDLE, THREADINFOCLASS, PVOID, ULONG, PULONG);
+
+//     const auto fn = reinterpret_cast<Fn>(
+//         GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtQueryInformationThread"));
+//     if (fn == nullptr) {
+//         die("GetProcAddress(NtQueryInformationThread)");
+//     }
+
+//     PVOID start = nullptr;
+//     const NTSTATUS st =
+//         fn(thread, ThreadQuerySetWin32StartAddress, &start, sizeof(start), nullptr);
+//     if (st < 0) {  // !NT_SUCCESS
+//         die("NtQueryInformationThread");
+//     }
+//     return reinterpret_cast<DWORD64>(start);
+// }
 
 }  // namespace
 
@@ -48,9 +175,9 @@ int main(int argc, char* argv[]) {
     }
 
     const auto pid = static_cast<DWORD>(std::strtoul(argv[1], nullptr, 10));
-    const auto tid = static_cast<DWORD>(std::strtoul(argv[2], nullptr, 10));
+    //const auto tid = static_cast<DWORD>(std::strtoul(argv[2], nullptr, 10));
 
-    auto process = open_process(PROCESS_ALL_ACCESS, pid);
+    const auto process = uniq_handle{OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid)};
     if (!process) {
         die("OpenProcess");
     }
@@ -59,66 +186,9 @@ int main(int argc, char* argv[]) {
         die("SymInitialize");
     }
 
-    auto thread = open_thread(THREAD_ALL_ACCESS, tid);
-    if (!thread) {
-        die("OpenThread");
-    }
+    if (auto et_ret = enumThreads(process.get()) .and_then(enumThreadStacks); !et_ret)
+        die(std::format("enumThreads : {:x}", et_ret.error()));
 
-    if (SuspendThread(thread.get()) == static_cast<DWORD>(-1)) {
-        die("SuspendThread");
-    }
-
-    CONTEXT ctx{};
-    ctx.ContextFlags = CONTEXT_FULL;
-    if (!GetThreadContext(thread.get(), &ctx)) {
-        die("GetThreadContext");
-    }
-
-    STACKFRAME64 frame{};
-    frame.AddrPC.Offset = ctx.Rip;
-    frame.AddrFrame.Offset = ctx.Rbp;
-    frame.AddrStack.Offset = ctx.Rsp;
-    frame.AddrPC.Mode = AddrModeFlat;
-    frame.AddrFrame.Mode = AddrModeFlat;
-    frame.AddrStack.Mode = AddrModeFlat;
-
-    while (true) {
-        if (!StackWalk64(IMAGE_FILE_MACHINE_AMD64,
-                         process.get(),
-                         thread.get(),
-                         &frame,
-                         &ctx,
-                         nullptr,
-                         SymFunctionTableAccess64,
-                         SymGetModuleBase64,
-                         nullptr))
-            break;
-
-        const auto address = frame.AddrPC.Offset;
-        if (address == 0) break;
-
-        //auto* symbol = static_cast<PSYMBOL_INFO>(
-        alignas(SYMBOL_INFO) std::array<CHAR, (sizeof(SYMBOL_INFO) + (MAX_PATH + 1) * sizeof(CHAR))> symbol_buff{};
-        auto* symbol = reinterpret_cast<PSYMBOL_INFO>(symbol_buff.data());
-        symbol->MaxNameLen = MAX_PATH;
-        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-
-        DWORD64 disp{};
-        IMAGEHLP_MODULE64 mod{};
-        mod.SizeOfStruct = sizeof(mod);
-        if (SymFromAddr(process.get(), address, &disp, symbol)) {
-            if (SymGetModuleInfo64(process.get(), address, &mod))
-                std::println("{}!{}+0x{:x}", mod.ModuleName, symbol->Name, disp);
-            else
-                std::println("<UNKNOWN>!{}+0x{:x}", symbol->Name, disp);
-        } else {
-            if (SymGetModuleInfo64(process.get(), address, &mod))
-                std::println("{}+{:#x}", mod.ModuleName, address - mod.BaseOfImage);
-            else
-                std::println("{:#x}", address);
-        }
-    }
     SymCleanup(process.get());
-    ResumeThread(thread.get());
     return EXIT_SUCCESS;
 }
